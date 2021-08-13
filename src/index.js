5838f92e3ce24c248075e817ed1b538fc676f968
Jincheng Zhang <mambainustc@gmail.com>
2021-08-12 22:59:40 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -206,7 +206,7 @@ function createSlidePlayer() {
     slides: slides,
     walk: function (index, slideSettings) {
       $("#counter").html(index + 1);
-      if ($("#counter").html() > IMAGE_COUNT - 1 && !LOOP_SLIDES) { 
+      if ($("#counter").html() > 520 && !LOOP_SLIDES) { 
         // when jumping to the end then go back a few slides and then keeps palying till the end, the slides will still stop at the end instead of looping
         player.pause();
         $("#pause").hide();
